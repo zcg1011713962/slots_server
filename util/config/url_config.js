a42b889f3d4f18fa235146888d6c99b4fc2b4a59
wenhua_wei <wenhua_wei@126.com>
2024-03-19 10:04:04 +0800

    serverUrl 设置为环境变量

--- util/config/url_config.js
+++ util/config/url_config.js
@@ -1,6 +1,6 @@
 
 const serverUrl = {};
-serverUrl.hall_url =  'http://localhost:13000'
-serverUrl.broadcast_ip =  '127.0.0.1'
+serverUrl.hall_url =  process.env.HALL_URL||'http://localhost:13000'
+serverUrl.broadcast_ip =  process.env.BROADCAST_IP||'127.0.0.1'
 
 module.exports = serverUrl;
